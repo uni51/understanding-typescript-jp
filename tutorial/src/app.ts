badd0740db18dc674afd66c7f3915252e8ab6f81
uni51 <uni51@users.noreply.github.com>
2020-09-14 08:42:10 +0900

    87. 関数オーバーロード

--- tutorial/src/app.ts
+++ tutorial/src/app.ts
@@ -25,88 +25,96 @@ type Numeric = number | boolean;
 // CombinableとNumericの共通部分になるので、結果的に、number型のみの指定となる。
 type Universal = Combinable & Numeric;
 
+// 関数オーバーロード ------------------------------
+function add(a: number, b: number): number;
+function add(a: string, b: string): string;
+function add(a: string, b: number): string;
+function add(a: number, b: string): string;
 function add(a: Combinable, b: Combinable) {
   // 型（タイプ）ガード
   if (typeof a === "string" || typeof b === "string") {
-    return a.toString() + b.toString();
+    return a.toString() + b?.toString();
   }
   return a + b;
 }
 
-type UnknownEmployee = Employee | Admin;
+const result = add("Hello", " TypeScript");
+result.split(" ");
 
-function printEmployeeInformation(emp: UnknownEmployee) {
-  console.log(emp.name);
-  // 型（タイプ）ガード
-  if ("privileges" in emp) {
-    console.log("Privileges: " + emp.privileges);
-  }
-  if ("startDate" in emp) {
-    console.log("Start Date: " + emp.startDate);
-  }
-}
+// type UnknownEmployee = Employee | Admin;
 
-printEmployeeInformation({ name: "Manu", startDate: new Date() });
+// function printEmployeeInformation(emp: UnknownEmployee) {
+//   console.log(emp.name);
+//   // 型（タイプ）ガード
+//   if ("privileges" in emp) {
+//     console.log("Privileges: " + emp.privileges);
+//   }
+//   if ("startDate" in emp) {
+//     console.log("Start Date: " + emp.startDate);
+//   }
+// }
 
-class Car {
-  drive() {
-    console.log("運転中...");
-  }
-}
+// printEmployeeInformation({ name: "Manu", startDate: new Date() });
 
-class Truck {
-  drive() {
-    console.log("トラックを運転中...");
-  }
+// class Car {
+//   drive() {
+//     console.log("運転中...");
+//   }
+// }
 
-  loadCargo(amount: number) {
-    console.log("荷物を載せています..." + amount);
-  }
-}
+// class Truck {
+//   drive() {
+//     console.log("トラックを運転中...");
+//   }
 
-type Vehicle = Car | Truck;
+//   loadCargo(amount: number) {
+//     console.log("荷物を載せています..." + amount);
+//   }
+// }
 
-const v1 = new Car();
-const v2 = new Truck();
+// type Vehicle = Car | Truck;
 
-function useVehicle(vehicle: Vehicle) {
-  vehicle.drive();
-  // 型（タイプ）ガード
-  if (vehicle instanceof Truck) {
-    vehicle.loadCargo(1000);
-  }
-}
+// const v1 = new Car();
+// const v2 = new Truck();
 
-useVehicle(v1);
-useVehicle(v2);
+// function useVehicle(vehicle: Vehicle) {
+//   vehicle.drive();
+//   // 型（タイプ）ガード
+//   if (vehicle instanceof Truck) {
+//     vehicle.loadCargo(1000);
+//   }
+// }
 
-interface Bird {
-  type: "bird";
-  flyingSpeed: number;
-}
+// useVehicle(v1);
+// useVehicle(v2);
 
-interface Horse {
-  type: "horse";
-  runningSpeed: number;
-}
+// interface Bird {
+//   type: "bird";
+//   flyingSpeed: number;
+// }
 
-// 判別可能なUnion型 ------------------------------
-type Animal = Bird | Horse;
+// interface Horse {
+//   type: "horse";
+//   runningSpeed: number;
+// }
 
-function moveAnimal(animal: Animal) {
-  let speed;
-  switch (animal.type) {
-    case "bird":
-      speed = animal.flyingSpeed;
-      break;
-    case "horse":
-      speed = animal.runningSpeed;
-      break;
-  }
-  console.log("移動速度: " + speed);
-}
+// // 判別可能なUnion型 ------------------------------
+// type Animal = Bird | Horse;
 
-moveAnimal({ type: "bird", flyingSpeed: 10 });
+// function moveAnimal(animal: Animal) {
+//   let speed;
+//   switch (animal.type) {
+//     case "bird":
+//       speed = animal.flyingSpeed;
+//       break;
+//     case "horse":
+//       speed = animal.runningSpeed;
+//       break;
+//   }
+//   console.log("移動速度: " + speed);
+// }
+
+// moveAnimal({ type: "bird", flyingSpeed: 10 });
 
 // 型キャスト ------------------------------
 
