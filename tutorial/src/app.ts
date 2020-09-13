f72be818e2461a98d80572a25bb9931bc75172e0
uni51 <uni51@users.noreply.github.com>
2020-09-13 09:48:10 +0900

    84. 判別可能な Union 型

--- tutorial/src/app.ts
+++ tutorial/src/app.ts
@@ -79,3 +79,30 @@ function useVehicle(vehicle: Vehicle) {
 
 useVehicle(v1);
 useVehicle(v2);
+
+interface Bird {
+  type: "bird";
+  flyingSpeed: number;
+}
+
+interface Horse {
+  type: "horse";
+  runningSpeed: number;
+}
+
+type Animal = Bird | Horse;
+
+function moveAnimal(animal: Animal) {
+  let speed;
+  switch (animal.type) {
+    case "bird":
+      speed = animal.flyingSpeed;
+      break;
+    case "horse":
+      speed = animal.runningSpeed;
+      break;
+  }
+  console.log("移動速度: " + speed);
+}
+
+moveAnimal({ type: "bird", flyingSpeed: 10 });
