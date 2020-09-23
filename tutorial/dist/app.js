ca07ccf5741d5622648a1fbe01dbbca1136fdd59
uni51 <uni51@users.noreply.github.com>
2020-09-24 06:33:06 +0900

    94. Generic 関数の作成

--- tutorial/dist/app.js
+++ tutorial/dist/app.js
@@ -1,3 +1,7 @@
 "use strict";
-var names = [];
+function merge(objA, objB) {
+    return Object.assign(objA, objB);
+}
+const mergeObj = merge({ name: "Max", hobbies: ["Sports"] }, { age: 30 });
+console.log(mergeObj.age);
 //# sourceMappingURL=app.js.map
